a8c3d580f8d74355eb3a74c868ad7057cd900634
Jameson Watts <jameson.watts@gmail.com>
2020-02-01 09:05:45 -0800

    Yay!

---
+++ resources/pinot.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+setwd("~/GDrive/teaching/GSMDS-5004/resources")
+read_rds("wine.rds") %>% 
+  filter(variety %in% c("Pinot Noir","Burgundy","Pinot Nero")) %>% 
+  mutate(province = fct_lump(province,5)) %>% 
+  mutate(province = as.character(province)) %>% 
+  filter(province != "Other") %>% 
+  select(province, price, points, year, taster_name, description) %>% 
+  drop_na() %>% 
+  mutate(province = str_replace_all(province, " ", "_")) %>% 
+  write_rds(path = "pinot.rds")
