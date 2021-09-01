bc6a1976d27990d490407b3b16d1ee7739984378
Jameson Watts <jameson.watts@gmail.com>
2021-09-01 15:27:55 -0700

    Updated!

--- resources/pinot.R
+++ resources/pinot.R
@@ -1,11 +1,13 @@
 library(tidyverse)
-setwd("~/GDrive/teaching/GSMDS-5004/resources")
-read_rds("wine.rds") %>% 
+setwd("~/GDrive/teaching/DATA-504/resources")
+wine <- read_rds("wine.rds") %>% 
   filter(variety %in% c("Pinot Noir","Burgundy","Pinot Nero")) %>% 
   mutate(province = fct_lump(province,5)) %>% 
   mutate(province = as.character(province)) %>% 
   filter(province != "Other") %>% 
   select(province, price, points, year, taster_name, description) %>% 
   drop_na() %>% 
-  mutate(province = str_replace_all(province, " ", "_")) %>% 
+  mutate(province = str_replace_all(province, " ", "_")) 
+
+as.data.frame(wine) %>% 
   write_rds(path = "pinot.rds")
--- resources/variety.R
+++ resources/variety.R
@@ -1,14 +1,16 @@
 library(tidyverse)
-setwd("~/GDrive/teaching/GSMDS-5004/resources")
+setwd("~/GDrive/teaching/DATA-504/resources")
 
 
 wine <- read_rds("wine.rds") %>% 
   filter(province=="Oregon") %>% 
-  mutate(variety = fct_lump(variety,5)) %>% 
+  mutate(variety = fct_lump(variety,3)) %>% 
   filter(variety != "Other") %>% 
   select(variety, price, points, description) %>% 
   drop_na() %>% 
   mutate(variety = str_replace_all(variety, " ", "_"))
 
-write_rds(wine, path="variety-project.rds")
+write_rds(wine, file="variety.rds")
+
+
 
